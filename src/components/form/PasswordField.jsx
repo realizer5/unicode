00be50e816f85637ff0a3c4bd8e7583be8e5f0cc
realizer <ipreet35232@gmail.com>
2025-03-09 13:43:56 +0530

    minor update

--- src/components/form/PasswordField.jsx
+++ src/components/form/PasswordField.jsx
@@ -3,17 +3,14 @@ import { Eye, EyeOff } from "lucide-react";
 
 const PasswordField = ({ inputLabel = "Password" }) => {
     const [isClose, setIsClose] = useState(true);
+
     return (
         <div className="mb-2">
-            <label
-                htmlFor="password"
-                className="block text-sm font-semibold text-gray-800"
-            >
+            <label htmlFor="password" className="block text-sm font-semibold text-gray-800" >
                 {inputLabel}
             </label>
             <div className='relative mt-2 flex justify-end items-center'>
-                <input
-                    type={isClose ? "password" : "text"}
+                <input type={isClose ? "password" : "text"}
                     className="block w-full px-4 py-2 pe-10 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40 user-invalid-pink"
                     required
                 />
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
 import { Uni, UniInside, UniStar } from '../../img'
 
 export default function Home() {
     return (
         <>
-            <div className='flex overflow-hidden'>
-                <img src={Uni} alt="" />
-                <img src={UniInside} alt="" />
-                <img src={UniStar} alt="" />
+            <div className='overflow-hidden relative flex justify-center'>
+                <div className='flex'>
+                    <img src={Uni} alt="" />
+                    <img src={UniInside} alt="" />
+                    <img src={UniStar} alt="" />
+                </div>
+                <div className='absolute bottom-0 space-x-2'>
+                    <button className='w-8 h-1 bg-gray-900'></button>
+                    <button className='w-8 h-1 bg-gray-900'></button>
+                    <button className='w-8 h-1 bg-gray-900'></button>
+                </div>
             </div>
         </>
     )
